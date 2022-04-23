85511732e7d8408314eefee40b071a116d39759b
zhouyuqian <i@dounait.cn>
2022-04-23 19:58:05 +0800

    update git to https

--- linux terminal/init.sh
+++ linux terminal/init.sh
@@ -7,7 +7,7 @@ echo " install oh-my-zsh"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo " download zsh plugins"
-git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo " chang oh-my-zsh conf"

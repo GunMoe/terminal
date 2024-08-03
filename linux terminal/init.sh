13ced2733e7b643d7aa30892a18899f899a55cd2
i <i@ivm-dev.lan>
2024-08-03 21:08:02 +0800

    add tmux config

--- linux terminal/init.sh
+++ linux terminal/init.sh
@@ -10,6 +10,8 @@ echo " download zsh plugins"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 echo " chang oh-my-zsh conf"
 sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"ys\"/' ~/.zshrc
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
